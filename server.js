3b1cc907390f976a7ca22d71a0aa23810c9f52f5
yanolsh <yanolsh@bu.edu>
2017-03-28 19:55:18 -0400

    Add project page

--- server.js
+++ server.js
@@ -51,7 +51,11 @@ app.get('/about',(req,res)=>{
     pageTitle:'About Page',
   });
 });
-
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
+});
 // /bad
 
 app.get('/bad',(req,res)=>{
